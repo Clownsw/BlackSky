46243bce18f429d7fa096e890725d2ed6960de68
Smliexx <msmliexx1@gmail.com>
2021-12-26 19:37:07 +0800

    删除fast_io
    1. 删除fast_io
    2. 不使用c++20

--- c/include/fast_io/fast_io_core_impl/concat/impl.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include"strlike_reference_wrapper.h"
-#include"concat_buffer.h"
-#include"concat_general.h"
--- c/include/fast_io/fast_io_core_impl/simd/impl.h
+++
@@ -1,5 +0,0 @@
-﻿#pragma once
-
-#if (defined(__GNUC__) || defined(__clang__))
-#include"x86_64.h"
-#endif
